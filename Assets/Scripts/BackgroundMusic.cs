7d6e3b6dee7b873551f11b959c3f6a08550cd833
Alejandro <cesar.leo14@yahoo.com>
2018-05-07 10:47:07 -0500

    Background Music & Audio Mix
    Added background music, credited them in the credits page, and adjust audio levels for sfx.

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
